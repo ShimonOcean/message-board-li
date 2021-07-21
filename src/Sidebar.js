019cac092f5f3bc12ea49337ca153a553d9675a8
ShimonIslam <shimon.ocean@gmail.com>
2021-07-20 20:08:12 -0700

    Sidebar html + styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,43 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
+import "./Sidebar.css";
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className="sidebar_recentItem">
+            <span className="sidebar_hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
+
     return (
         <div className='sidebar'>
-           <h1>I am sidebar</h1>     
+           <div className="sidebar_top">
+               <img src="https://wallpaperaccess.com/full/267434.jpg" alt=""/>
+               <Avatar />
+               <h2>your name</h2>
+               <h4>your@email.com</h4>
+            </div>   
+
+            <div className="sidebar_stats">
+                <div className="sidebar_stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar_statNumber">178</p>
+                </div>
+                <div className="sidebar_stat">
+                    <p>Views on post</p>
+                    <p className="sidebar_statNumber">21</p>
+                </div>
+            </div>
+            <div className="sidebar_bottom">
+                <p>Recent</p>
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('javascript')}
+            </div>
         </div>
     )
 }
